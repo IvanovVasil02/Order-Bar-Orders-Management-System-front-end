80ca53bf4981f6e8ea98dd41c81501b45e6dfdee
IvanovVasil02 <ivanov.vasil235@gmail.com>
2024-07-11 17:01:54 +0200

    a

--- src/components/redux/actions/config/apiConfig.js
+++ src/components/redux/actions/config/apiConfig.js
@@ -1,2 +1,2 @@
-// export const APIBASE = "https://bar-order-management-system-production.up.railway.app/";
-export const APIBASE = "http://localhost:3001";
+export const APIBASE = "https://bar-order-management-system-production.up.railway.app/";
+// export const APIBASE = "http://localhost:3001";
